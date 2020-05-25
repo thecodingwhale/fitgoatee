fc13bede98b6b64e3ebbd7c40184052a387ec94c
Aldren Terante <aldren.terante@gmail.com>
2020-05-25 16:14:06 +0800

    feat: add a working subscription with api integration

--- src/components/subscribe-form.js
+++ src/components/subscribe-form.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import classnames from 'classnames'
 import * as yup from 'yup'
+import axios from 'axios'
 
 const SubscribeFormSchema = yup.object().shape({
   firstName: yup.string().required(),
@@ -9,16 +10,97 @@ const SubscribeFormSchema = yup.object().shape({
   emailAddress: yup.string().email().required(),
 })
 
+const postSubscribe = async ({ firstName, lastName, emailAddress }) => {
+  const url =
+    'https://5ktxoztqqa.execute-api.us-east-2.amazonaws.com/fitgoatee/send-email'
+  const payload = {
+    payload: {
+      firstName,
+      lastName,
+      emailAddress,
+    },
+  }
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, cors, *same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'omit', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow', // manual, *folslow, error
+    referrer: 'client', // no-referrer, *client
+    body: JSON.stringify(payload), // body data type must match "Content-Type" header
+  })
+  const json = await response.json().then(response)
+  return json
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 const SubscribeForm = () => {
-  const { register, handleSubmit, errors } = useForm({
+  const [submitting, setSubmitting] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const [error, setError] = useState(false)
+  const { register, handleSubmit, errors, reset } = useForm({
     validationSchema: SubscribeFormSchema,
   })
-  const onSubmit = (data) => console.log(data)
-  console.log('errors: ', errors)
+  const onSubmit = async (data) => {
+    const { firstName, lastName, emailAddress } = data
+    setSubmitting(true)
+    setSuccess(false)
+    setError(false)
+    await sleep(500)
+    try {
+      setSubmitting(false)
+      const response = await postSubscribe({
+        firstName,
+        lastName,
+        emailAddress,
+      })
+      if (response.statusCode === 200) {
+        setSuccess(true)
+        reset({
+          firstName: '',
+          lastName: '',
+          emailAddress: '',
+        })
+      } else {
+        setError(response.body)
+      }
+    } catch (error) {
+      setSubmitting(false)
+      setError('Something when wrong.')
+    }
+  }
+
   return (
     <form className="w-full max-w-lg" onSubmit={handleSubmit(onSubmit)}>
+      {success && (
+        <div
+          className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-6"
+          role="alert"
+        >
+          <span className="block sm:inline">Thank you for subscribing!</span>
+        </div>
+      )}
+      {error && (
+        <div
+          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-6"
+          role="alert"
+        >
+          <span className="block sm:inline">{error}</span>
+        </div>
+      )}
       <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <div
+          className={classnames({
+            'w-full md:w-1/2 px-3 mb-6 md:mb-0': true,
+            'opacity-50': submitting,
+          })}
+        >
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="grid-first-name"
@@ -26,6 +108,7 @@ const SubscribeForm = () => {
             First Name
           </label>
           <input
+            disabled={submitting}
             className={classnames({
               'appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white': true,
               'border-red-500':
@@ -45,7 +128,12 @@ const SubscribeForm = () => {
             </p>
           )}
         </div>
-        <div className="w-full md:w-1/2 px-3">
+        <div
+          className={classnames({
+            'w-full md:w-1/2 px-3': true,
+            'opacity-50': submitting,
+          })}
+        >
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="grid-last-name"
@@ -53,6 +141,7 @@ const SubscribeForm = () => {
             Last Name
           </label>
           <input
+            disabled={submitting}
             className={classnames({
               'appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white': true,
               'border-red-500':
@@ -71,7 +160,12 @@ const SubscribeForm = () => {
           )}
         </div>
       </div>
-      <div className="flex flex-wrap -mx-3 mb-2">
+      <div
+        className={classnames({
+          'flex flex-wrap -mx-3 mb-2': true,
+          'opacity-50': submitting,
+        })}
+      >
         <div className="w-full px-3 mb-6 md:mb-0">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -80,6 +174,7 @@ const SubscribeForm = () => {
             Email Address
           </label>
           <input
+            disabled={submitting}
             className={classnames({
               'appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white': true,
               'border-red-500':
@@ -99,10 +194,14 @@ const SubscribeForm = () => {
         </div>
       </div>
       <button
+        disabled={submitting}
         type="submit"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className={classnames({
+          'bg-blue-500 text-white font-bold py-2 px-4 rounded': true,
+          'opacity-50 cursor-not-allowed': submitting,
+        })}
       >
-        Subscribe
+        {submitting ? 'Subscribing...' : 'Subscribe'}
       </button>
     </form>
   )
