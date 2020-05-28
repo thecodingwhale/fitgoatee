94f9d834d567090d8bb7b604873d88d62cdbbfac
Aldren Terante <aldren.terante@gmail.com>
2020-05-28 22:34:06 +0800

    feat: add dotenv to upper pagE

--- src/components/subscribe-form.js
+++ src/components/subscribe-form.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 import React, { useState, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import classnames from 'classnames'
