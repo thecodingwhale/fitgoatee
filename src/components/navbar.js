810a6d3c794b20ab3bcbb8789e54a344597b51de
Aldren Terante <aldren.terante@gmail.com>
2020-06-09 08:32:02 +0800

    feat: add static RecipeCard

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,64 +5,54 @@ import Logo from '../assets/images/fitgoatee.svg'
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <header className="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
-      <div className="flex items-center justify-between px-4 py-3 sm:p-0">
-        <div>
-          <Logo className="h-8 w-20" />
+    <header className="bg-gray-900 mb-4">
+      <div className="container mx-auto sm:px-4 sm:py-3 sm:flex sm:justify-between sm:items-center">
+        <div className="flex items-center justify-between px-4 py-3 sm:p-0">
+          <div>
+            <Logo className="h-8 w-20" />
+          </div>
+          <div className="sm:hidden">
+            <button
+              onClick={() => {
+                setIsOpen(!isOpen)
+              }}
+              type="button"
+              className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+            >
+              <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                <path
+                  className={classnames({
+                    hidden: !isOpen,
+                  })}
+                  fillRule="evenodd"
+                  d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                />
+                <path
+                  className={classnames({
+                    hidden: isOpen,
+                  })}
+                  fillRule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-        <div className="sm:hidden">
-          <button
-            onClick={() => {
-              setIsOpen(!isOpen)
-            }}
-            type="button"
-            className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+        <nav
+          className={classnames({
+            'px-2 pt-2 pb-4 sm:flex sm:p-0': true,
+            block: isOpen,
+            hidden: !isOpen,
+          })}
+        >
+          <a
+            href="#"
+            className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
           >
-            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-              <path
-                className={classnames({
-                  hidden: !isOpen,
-                })}
-                fill-rule="evenodd"
-                d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-              />
-              <path
-                className={classnames({
-                  hidden: isOpen,
-                })}
-                fill-rule="evenodd"
-                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-              />
-            </svg>
-          </button>
-        </div>
+            Recipes
+          </a>
+        </nav>
       </div>
-      <nav
-        className={classnames({
-          'px-2 pt-2 pb-4 sm:flex sm:p-0': true,
-          block: isOpen,
-          hidden: !isOpen,
-        })}
-      >
-        <a
-          href="#"
-          className="block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800"
-        >
-          Recipes
-        </a>
-        <a
-          href="#"
-          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
-        >
-          Blogs
-        </a>
-        <a
-          href="#"
-          className="mt-1 block px-2 py-1 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
-        >
-          About
-        </a>
-      </nav>
     </header>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,14 +20,51 @@ class RootIndex extends React.Component {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const apiUrl = get(this, 'props.data.env.apiUrl')
     const metaDescription = `My main goal is to help those who want to lose weight by guiding them where to start, categorize and share all my personal experiences in this journey.`
+    const elements = ['one', 'two', 'three', 'four']
     return (
-      <div>
+      <div className="antialiased">
         <Helmet>
           <title>{siteTitle}</title>
           <meta name="description" content={metaDescription} />
         </Helmet>
         <Navbar />
-        <div className={styles.box}>
+        <div className="container mx-auto px-4">
+          <h2>Recipes</h2>
+          <p className="mb-4">
+            My everyday home cooked meals for my weight loss
+          </p>
+          <div className="grid gap-6 grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
+            {elements.map((value, index) => {
+              return (
+                <div key={index}>
+                  <div className="relative pb-5/6">
+                    <img
+                      className="absolute h-full w-full object-cover rounded-lg shadow-md"
+                      src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+                      alt="Rear view of modern home with pool"
+                    />
+                  </div>
+                  <div className="relative px-4 -mt-16">
+                    <div className="bg-white p-4 rounded-lg shadow-lg">
+                      <div className="flex items-baseline">
+                        <div className="text-gray-600 text-xs uppercase font-semibold tracking-wide">
+                          Day 01
+                        </div>
+                      </div>
+                      <h4 className="mt-1 font-semibold text-sm lg:text-md leading-tight truncate">
+                        Modern home in city center Modern home in city center
+                      </h4>
+                      <div className="mt-1 text-xs truncate">
+                        This is a short description of the meal
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        {/* <div className={styles.box}>
           <h1 className="mb-6">
             <Logo />
             <span className="hidden">Fitgoatee</span>
@@ -76,7 +113,7 @@ class RootIndex extends React.Component {
               </a>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     )
   }
