8f2adc68c827d80ef39651b54a7126e7d24b9ce5
Aldren Terante <aldren.terante@gmail.com>
2020-06-11 10:33:36 +0800

    feat: update template

--- src/components/template.js
+++ src/components/template.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+// This ensures that the icon CSS is loaded immediately before attempting to render icons
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+
+// Prevent fontawesome from dynamically adding its css since we did it manually above
+config.autoAddCss = false
 
 const Template = ({ children }) => {
   return (
-    <div className="relative">
+    <div className="relative antialiased">
       <Navbar />
       {children}
     </div>
