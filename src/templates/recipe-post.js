e2a133a23da3897fb3f77cc44f0fc9738fb2521b
Aldren Terante <aldren.terante@gmail.com>
2020-06-17 10:09:38 +0800

    feat: extract recipe css from recipe template

--- src/templates/recipe-post.js
+++ src/templates/recipe-post.js
@@ -9,31 +9,26 @@ class Recipe extends React.Component {
   render() {
     const post = get(this.props, 'data.contentfulBlogPost')
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    console.log('post: ', post)
     return (
       <Template>
-        <div className="relative pb-4/6 sm:pb-4/12 mb-6">
+        <div className="recipe-cover">
           <img
-            className="absolute h-full w-full object-cover shadow-md"
+            className="recipe-image"
             src={post.heroImage.fluid.src}
             alt={post.heroImage.description}
           />
         </div>
         <div className="container mx-auto px-4 mb-6">
-          <div className="sm:absolute sm:top-0 sm:mt-24 sm:bg-black sm:bg-opacity-50 sm:max-w-md sm:rounded-lg sm:px-4 sm:py-4 md:px-6 md:py-6">
-            <h1 className="text-gray-900 text-2xl font-semibold mb-2 sm:text-white sm:mb-0 md:text-2xl md:text-4xl">
-              {post.title}
-            </h1>
-            <p className="text-gray-800 leading-relaxed mb-4 sm:text-white sm:mb-0 md:text-lg">
+          <div className="recipe-lead-container">
+            <h1 className="recipe-lead-title">{post.title}</h1>
+            <p className="recipe-lead-description">
               {post.description.description}
             </p>
           </div>
-          <div className="sm:flex">
-            <div className="sm:w-2/5">
-              <h2 className="text-gray-900 text-xl font-semibold mb-2 md:text-2xl">
-                Ingredients
-              </h2>
-              <ul className="list-disc list-inside text-gray-800 leading-relaxed mb-4 sm:text-xs md:text-sm lg:text-base">
+          <div className="recipe-content">
+            <div className="recipe-sidebar">
+              <h2>Ingredients</h2>
+              <ul>
                 <li>1½ tbsp unflavored powdered gelatin</li>
                 <li>¼ cup cold water</li>
                 <li>1½ cups unsweetened almond milk</li>
@@ -43,62 +38,56 @@ class Recipe extends React.Component {
                 <li>4 egg yolks, large eggs</li>
               </ul>
             </div>
-            <div className="sm:w-3/5">
-              <h2 className="text-gray-900 text-xl font-semibold mb-2 md:text-2xl">
-                Instructions
-              </h2>
-              <ol className="list-decimal list-inside text-gray-800 leading-relaxed mb-4">
-                <li className="mb-2">
+            <div className="recipe-body">
+              <h2>Instructions</h2>
+              <ol>
+                <li>
                   In a small bowl, stir together the gelatin and cold water. Set
                   aside for 3-5 minutes, giving the gelatin a chance to bloom.
                 </li>
-                <li className="mb-2">
+                <li>
                   Add the almond milk, heavy cream, and vanilla to a
                   medium-sized saucepan, stirring together constantly, over
                   medium heat. When bubbles begin to form, remove from the heat,
                   and add the bloomed gelatin to the hot mixture. Whisk together
                   until the gelatin has completely dissolved.
                 </li>
-                <li className="mb-2">
+                <li>
                   Place the erythritol and egg yolks into a mixing bowl. Using
                   an electric mixer, beat together at medium speed until it
                   becomes a light color. Reduce the speed to low and very
                   slowly, trickle the milk mixture into the egg mixture,
                   stirring for a few minutes, until combined.
                 </li>
-                <li className="mb-2">
+                <li>
                   Add the mixture back to the saucepan, and heat over medium-low
                   heat, whisking constantly for about 5 minutes.
                 </li>
-                <li className="mb-2">
+                <li>
                   Remove from the heat, and place the custard in a heat-safe
                   bowl. Refrigerate for about 1/2 hour, or until completely
                   cooled, and slightly thickened. Begin preparing the pastry.
                 </li>
               </ol>
-              <h2 className="text-gray-900 text-xl font-semibold mb-2 md:text-2xl">
-                Tips
-              </h2>
-              <p className="text-gray-800 leading-relaxed mb-2">
+              <h2>Tips</h2>
+              <p>
                 We do not recommend scaling up or down the serving size of this
                 recipe due to its complexity.
               </p>
-              <p className="text-gray-800 leading-relaxed mb-2">
+              <p>
                 Pastry will soften quite fast, so this slice is best eaten
                 quickly.
               </p>
-              <p className="text-gray-800 leading-relaxed mb-2">
+              <p>
                 If you prefer a sturdier pastry, double the quantity and roll it
                 thicker.
               </p>
-              <p className="text-gray-800 leading-relaxed mb-2">
+              <p>
                 Little cracks may form in the dough while being rolled out.
                 Simply take dough pieces from the edges, patch the cracks, and
                 roll out to smooth.
               </p>
-              <p className="text-gray-800 leading-relaxed mb-2">
-                Refrigerates well for a few days.
-              </p>
+              <p>Refrigerates well for a few days.</p>
             </div>
           </div>
         </div>
